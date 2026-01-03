3479b62ec74c5c06be93a6938843cebc2de55143
Gabi Lecusay <112031715+senjerak@users.noreply.github.com>
2026-01-03 14:54:52 -0500

    1.0.1

---
+++ scripts/loader.js
@@ -0,0 +1,8 @@
+const MIN_LOADING_TIME = 1000;
+const start = Date.now()
+window.addEventListener('load', e => {
+    const delta = Date.now() - start
+    setTimeout(() => {
+        document.querySelector('.loader').remove()
+    }, Math.max(0, MIN_LOADING_TIME - delta))
+}, { once: true })
