27fbfa2c8baa8a434d3e60b8fdc09197e9090535
Angel Piscola <angelpiscola@gmail.com>
2026-04-04 10:51:10 -0400

    feat: add inactivity timer that redirects after 5m of no interaction

---
+++ public/scripts/story_inactivity.js
@@ -0,0 +1,33 @@
+(function () {
+  if (window.__labrintaStoryInactivity) return;
+  window.__labrintaStoryInactivity = true;
+
+  var INACTIVITY_MS = 5 * 60 * 1000; // 5 minutes
+  var REDIRECT = "/scenes/1";
+  var timerId;
+
+  function redirect() {
+    window.location.href = REDIRECT;
+  }
+
+  function resetTimer() {
+    clearTimeout(timerId);
+    timerId = setTimeout(redirect, INACTIVITY_MS);
+  }
+
+  var events = [
+    "mousemove",
+    "mousedown",
+    "keydown",
+    "scroll",
+    "touchstart",
+    "wheel",
+    "click",
+  ];
+  for (var i = 0; i < events.length; i++) {
+    document.addEventListener(events[i], resetTimer, { passive: true });
+  }
+
+  document.addEventListener("astro:page-load", resetTimer);
+  resetTimer();
+})();
